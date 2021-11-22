abeaaf3a3c856d2e126d2d3942b461d0c53f32a1
Nata <natalie.storozhuk@outlook.com>
2021-11-22 11:40:14 -0500

    added freezing and boiling points exercise

---
+++ CsharpTheory/FreezingAndBoilingPoints.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTheory
+{
+    class FreezingAndBoilingPoints
+    {
+        private double temperature;
+
+        public FreezingAndBoilingPoints(double temperature)
+        {
+
+            this.temperature = temperature;
+        }
+
+        public void SetTepmerature(double temperature)
+        {
+            this.temperature = temperature;
+        }
+
+        public double GetTemperature()
+        {
+            return temperature;
+        }
+
+        public bool IsEthylFreezing()
+        {
+
+            if (temperature <= -173.0)
+            {
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        public bool IsEthylBoiling()
+        {
+
+            if (temperature >= 172.0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool IsOxygenFreezing()
+        {
+
+            if (temperature <= -362.0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool IsOxygenBoiling()
+        {
+
+            if (temperature >= -306.0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool IsWaterFreezing()
+        {
+
+            if (temperature <= 32.0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool IsWaterBoiling()
+        {
+
+            if (temperature >= 212.0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
--- CsharpTheory/Program.cs
+++ CsharpTheory/Program.cs
@@ -6,6 +6,40 @@ namespace CsharpTheory
     {
         static void Main(string[] args)
         {
+            double temperature = 15;
+            FreezingAndBoilingPoints f = new FreezingAndBoilingPoints(temperature);
+
+            // Display elements will freeze.
+            if (f.IsEthylBoiling())
+            {
+                Console.WriteLine("Ethyl alcohol will freeze.");
+            }
+
+            if (f.IsOxygenFreezing())
+            {
+                Console.WriteLine("Oxygen will freeze.");
+            }
+
+            if (f.IsWaterFreezing())
+                Console.WriteLine("Water will freeze.");
+
+            // Display if elements will boil.
+            if (f.IsEthylFreezing())
+            {
+                Console.WriteLine("Ethyl alcohol will boil.");
+            }
+
+            if (f.IsOxygenBoiling())
+            {
+                Console.WriteLine("Oxygen will boil.");
+            }
+
+            if (f.IsWaterBoiling())
+            {
+                Console.WriteLine("Water will boil.");
+            }
+
+
             #region MonthDays
             //int month = 2;
             //int year = 1958;
