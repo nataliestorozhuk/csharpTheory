9f37adcd2084905f36392e3de58ff31fb7c2011c
Nata <natalie.storozhuk@outlook.com>
2021-11-23 10:58:16 -0500

    saving account

--- CsharpTheory/Program.cs
+++ CsharpTheory/Program.cs
@@ -6,39 +6,64 @@ namespace CsharpTheory
     {
         static void Main(string[] args)
         {
-            double temperature = 15;
-            FreezingAndBoilingPoints f = new FreezingAndBoilingPoints(temperature);
+            #region SavingAccount
+            double annualInterestRate = 0.5;
+            double startingBalance = 100.0;
+            double numOfMonths = 2;
+
+            SavingAccount savingAccount = new SavingAccount(startingBalance);
+            for (int i = 0; i < numOfMonths; i++)
+            {
+
+                Console.WriteLine("Enter amount deposited into the account during the month: ");
+                var deposit = double.Parse(Console.ReadLine());
+                savingAccount.Add(deposit);
+                Console.WriteLine("Enter amount withdrawn from the account during the month: ");
+                var withdrawn = double.Parse(Console.ReadLine());
+                savingAccount.Add(withdrawn);
+                                
+            }
+
+            Console.WriteLine(savingAccount.GetBalanceWithInterestRate()); 
+
+
+
+            #endregion
+
+            #region FreezingAndBoiling
+            //double temperature = 15;
+            //FreezingAndBoilingPoints f = new FreezingAndBoilingPoints(temperature);
 
             // Display elements will freeze.
-            if (f.IsEthylBoiling())
-            {
-                Console.WriteLine("Ethyl alcohol will freeze.");
-            }
+            //if (f.IsEthylBoiling())
+            //{
+            //    Console.WriteLine("Ethyl alcohol will freeze.");
+            //}
 
-            if (f.IsOxygenFreezing())
-            {
-                Console.WriteLine("Oxygen will freeze.");
-            }
+            //if (f.IsOxygenFreezing())
+            //{
+            //    Console.WriteLine("Oxygen will freeze.");
+            //}
 
-            if (f.IsWaterFreezing())
-                Console.WriteLine("Water will freeze.");
+            //if (f.IsWaterFreezing())
+            //    Console.WriteLine("Water will freeze.");
 
-            // Display if elements will boil.
-            if (f.IsEthylFreezing())
-            {
-                Console.WriteLine("Ethyl alcohol will boil.");
-            }
+            //// Display if elements will boil.
+            //if (f.IsEthylFreezing())
+            //{
+            //    Console.WriteLine("Ethyl alcohol will boil.");
+            //}
 
-            if (f.IsOxygenBoiling())
-            {
-                Console.WriteLine("Oxygen will boil.");
-            }
+            //if (f.IsOxygenBoiling())
+            //{
+            //    Console.WriteLine("Oxygen will boil.");
+            //}
 
-            if (f.IsWaterBoiling())
-            {
-                Console.WriteLine("Water will boil.");
-            }
-
+            //if (f.IsWaterBoiling())
+            //{
+            //    Console.WriteLine("Water will boil.");
+            //}
+            #endregion
 
             #region MonthDays
             //int month = 2;
---
+++ CsharpTheory/SavingAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTheory
+{
+    class SavingAccount
+    {
+
+        private double annualInterestRate;
+        private double balance;
+
+        public SavingAccount(double balance)
+        {
+            this.balance = balance;
+
+        }
+
+        public double Subtract(double subtractingAmount)
+        {
+            balance -= subtractingAmount;
+            return balance;
+        }
+
+        public double Add(double add)
+        {
+            balance += add;
+            return balance;
+        }
+
+        public double GetBalanceWithInterestRate()
+        {
+
+            double x = annualInterestRate / 12;
+            return balance * x;
+        }
+    }
+}
