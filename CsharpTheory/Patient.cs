d23c83dc6dae9a058ad1393334257f3bf599044e
Nata <natalie.storozhuk@outlook.com>
2021-11-25 15:34:26 -0500

    finished patient and procedure classes

--- CsharpTheory/Patient.cs
+++ CsharpTheory/Patient.cs
@@ -10,11 +10,29 @@ namespace CsharpTheory
     {
         public static void Run()
         {
+            double totalCharges;
+
             Patient patient = new Patient("Nata", "no middle name", "Stor", "12 Beyer ave.", "Phila", "PA", "19116", "Nick", "609-849-1648");
             Procedure procedure1 = new Procedure("Exam", "November 25th 2021", "Dr. Irvine", 250.00);
-            Procedure procedure2 = new Procedure("E-ray", "November 25th 2021", "Dr. Jamison", 500.00);
+            Procedure procedure2 = new Procedure("X-ray", "November 25th 2021", "Dr. Jamison", 500.00);
             Procedure procedure3 = new Procedure("Blood test", "November 25th 2021", "Dr. Smith", 200.00);
 
+            Console.WriteLine("First name: " + patient.FirstName + "\n" + "Middle name: " + patient.MiddleName + "\n" + "Last name " + patient.LastName + "\n" + 
+               "Patient's address: " + patient.Address + ", " + patient.City + ", " + patient.State + ", " + patient.ZipCode + "\n" + 
+                "Emergency contact name: " + patient.EmergencyName + "\n" + "Emergency contact phone number: " + patient.EmergencyPhoneNumber);
+            Console.WriteLine("");
+            Console.WriteLine("Procedure #1: " + "\t\t\t" + "Procedure #2: " + "\t\t\t" + "Procedure #3: ");
+            Console.WriteLine("_______________________________________________________________________________");
+            Console.WriteLine("Procedure name:" + procedure1.ProcedureName + "\t\t" + "Procedure name:" + procedure2.ProcedureName + 
+                "\t\t" + "Procedure name:" + procedure3.ProcedureName);
+            Console.WriteLine("Date:" + procedure1.ProcedureDate + "\t\t" + "Date:" + procedure2.ProcedureDate +
+               "\t\t" + "Date:" + procedure3.ProcedureDate);
+            Console.WriteLine("Practitioner:" + procedure1.PractitionerName + "\t\t" + "Practitioner:" + procedure2.PractitionerName +
+               "\t\t" + "Practitioner:" + procedure3.PractitionerName);
+            Console.WriteLine("Charge:" + procedure1.ProcedureCharges + "\t\t\t" + "Charge:" + procedure2.ProcedureCharges +
+               "\t\t\t" + "Charge:" + procedure3.ProcedureCharges);
+            Console.WriteLine("");
+            Console.WriteLine("Total charges are: " + (procedure1.ProcedureCharges + procedure2.ProcedureCharges + procedure3.ProcedureCharges));
         }
 
         public string FirstName { get; set; }
