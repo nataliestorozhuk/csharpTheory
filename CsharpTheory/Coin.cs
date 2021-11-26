4a29e1dd8c590a4511153cf371d785e912b72be8
Nata <natalie.storozhuk@outlook.com>
2021-11-26 14:44:11 -0500

    created coin class

---
+++ CsharpTheory/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTheory
+{
+    class Coin
+    {
+        public static void Run()
+        {
+
+        }
+
+        public string SideUp { get; set; }
+    }
+}
