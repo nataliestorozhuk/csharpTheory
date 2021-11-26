ca826c0c9f40b1161ea9fe5b4313a1f2d5880c1e
Nata <natalie.storozhuk@outlook.com>
2021-11-26 12:23:52 -0500

    added roulette pocket class

---
+++ CsharpTheory/RoulettePocket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTheory
+{
+    class RoulettePocket
+    {
+        public static void Run()
+        {
+
+        }
+
+        public int pocketNumber { get; set; }
+
+        public RoulettePocket(int pocketNumber)
+        {
+            this.pocketNumber = pocketNumber;
+        }
+    }
+}
