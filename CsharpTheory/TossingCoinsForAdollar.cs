d33c82c65fb8d6474a54a17020b857ed7157f9d5
Nata <natalie.storozhuk@outlook.com>
2021-11-28 15:21:15 -0500

    refactored TossingCoinsForAdollar

--- CsharpTheory/TossingCoinsForAdollar.cs
+++ CsharpTheory/TossingCoinsForAdollar.cs
@@ -8,21 +8,22 @@ namespace CsharpTheory
         {
             double balance = 0.0;
 
+            //Coin quarter = new Coin();
+            //Coin dime = new Coin();
+            //Coin nickel = new Coin();
 
-            Coin quarter = new Coin();
-            Coin dime = new Coin();
-            Coin nickel = new Coin();
+            Coin coins = new Coin();
 
             while (balance <= 1.0)
             {
-                double q = quarter.Toss() == "heads" ? 0.25 : 0.0;
-                double d = dime.Toss() == "heads" ? 0.10 : 0.0;
-                double n = nickel.Toss() == "heads" ? 0.05 : 0.0;
+                double quater = coins.Toss() == "heads" ? 0.25 : 0.0;
+                double dime = coins.Toss() == "heads" ? 0.10 : 0.0;
+                double nickle = coins.Toss() == "heads" ? 0.05 : 0.0;
 
-                balance += q + d + n;
-                Console.WriteLine(q);
-                Console.WriteLine(d);
-                Console.WriteLine(n);
+                balance += quater + dime + nickle;
+                Console.WriteLine(quater);
+                Console.WriteLine(dime);
+                Console.WriteLine(nickle);
 
             }
             Console.WriteLine();
