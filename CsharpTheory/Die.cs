fc13d2bdbe1b8c23d2deef091cd48a3b47070c11
Nata <natalie.storozhuk@outlook.com>
2021-11-30 12:51:56 -0500

    added some changes to the Die class

--- CsharpTheory/Die.cs
+++ CsharpTheory/Die.cs
@@ -7,24 +7,32 @@ namespace CsharpTheory
         public static void Run()
         {
             const int DIE1_SIDES = 6;
-        
             const int MAX_ROLLS = 5;
 
             Die die1 = new Die(DIE1_SIDES);
-            Die die2 = new Die(DIE2_SIDES);
 
-            Console.WriteLine("This simulates the rolling of a " + DIE1_SIDES + " sided die and a " + DIE2_SIDES + " sided die.");
+
+            Console.WriteLine("This simulates the rolling of a " + DIE1_SIDES + " sided die. ");
             Console.WriteLine("Initial value of the dice: ");
-            Console.WriteLine(die1.Value + " " + die2.Value);
+            Console.WriteLine(die1.Value);
 
-            Console.WriteLine("Rolling the dice " + MAX_ROLLS + " times.");
-
+            Console.WriteLine("Rolling the dice ");
+                        
             for (int i = 0; i < MAX_ROLLS; i++)
             {
-                die1.roll();
-                die2.roll();
+                //die1.roll();
+                Console.WriteLine("Do you want to fish more items? (press 'y' for yes or 'n' for no): ");
+                string answer = Console.ReadLine().ToUpper();
 
-                Console.WriteLine(die1.Value + " " + die2.Value);
+                if (answer == "Y")
+                {
+                    die1.roll();
+
+                    Console.WriteLine(die1.Value);
+                }
+                else
+
+                    return;
             }
 
         }
