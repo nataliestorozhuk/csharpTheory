7b418fb48608a38a3b4c74d4c64aa85f81fe2085
Nata <natalie.storozhuk@outlook.com>
2021-12-02 12:46:16 -0500

    created Payroll class

---
+++ Arrays/PayrollClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class PayrollClass
+    {
+        private int[] employeeId = { 5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 7580489 };
+        private int[] hours = new int[7];
+        private double[] payRate = new double[7];
+        private double[] wages = new double[7];
+
+
+
+        public int[] EmployeeId
+        {
+            get { return employeeId; }
+            set { employeeId = value; }
+        }
+        public int[] Hours
+        {
+            get { return hours; }
+            set { hours = value; }
+        }
+        public double[] PayRate
+        {
+            get { return payRate; }
+            set { payRate = value; }
+        }
+        public double[] Wages
+        {
+            get { return wages; }
+            set { wages = value; }
+        }
+
+        public double GetGrossPay(int employeeId)
+        {
+
+            return;   
+        }
+    }
+}
