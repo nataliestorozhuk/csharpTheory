58cda07500f7d303354dd34393c8992b4273e707
Nata <natalie.storozhuk@outlook.com>
2021-12-09 10:14:26 -0500

    added LargerThanN class

--- Arrays/ChargeAccountValidation.cs
+++ Arrays/ChargeAccountValidation.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 
 namespace Arrays
@@ -11,18 +9,19 @@ namespace Arrays
         {
             int result;
 
-            int[] array = {5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 8080152, 4562555, 5552012, 5050552, 7825877, 1250255 };
+            int[] array = { 5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 8080152, 4562555, 5552012, 5050552, 7825877, 1250255 };
             //List<int> a = new List<int>();
-            string path = @"D:\OneDriveInn\OneDrive\Desktop\ChargeAccountModification.txt";
+            //string path = @"D:\OneDriveInn\OneDrive\Desktop\ChargeAccountModification.txt";
 
-            if (!File.Exists(path)) {
+            //if (!File.Exists(path))
+            //{
 
-                int[] createText = { 5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 8080152, 4562555, 5552012, 
-                    5050552, 7825877, 1250255 };
+            //    int[] createText = { 5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 8080152, 4562555, 5552012,
+            //        5050552, 7825877, 1250255 };
 
-                // Array.Convert()
-                File.WriteAllLines(path, Array.ConvertAll(createText, x => x.ToString()) );
-            }
+            //    // Array.Convert()
+            //    File.WriteAllLines(path, Array.ConvertAll(createText, x => x.ToString()));
+            //}
 
             Console.WriteLine("Enter the charge account number: ");
             int num = Convert.ToInt32(Console.ReadLine());
@@ -56,6 +55,10 @@ namespace Arrays
 
 
             return element;
+        }
+
+        public static int GetGreaterThenN(int[] array, int value) { 
+        
         }
     }
 }
---
+++ Arrays/LargerThanN.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Arrays
+{
+    class LargerThanN
+    {
+        public static void Run()
+        {
+            int result;
+
+            int[] array = { 5, 15, 7, 21, 97, 3, 48, 23 };
+
+            Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            result = GetGreaterThenN(array, num);
+
+        }
+
+        public static int SequantialSearch(int[] array, int value)
+        {
+            int index = 0;          //Loop control variable.
+            int element = -1;      //Element the value is found at.
+            bool found = false;   //Flag indicating search result.
+
+            //Search the array.
+            while (!found && index < array.Length)
+            {
+                if (array[index] == value)
+                {
+                    found = true;
+                    element = index;
+                }
+                index++;
+            }
+
+
+            return element;
+        }
+
+        public static int GetGreaterThenN(int[] array, int n)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (n > array[i])
+                {
+                    Console.WriteLine("\nn");
+                }
+            }
+            return 1;
+        }
+    }
+}
+
