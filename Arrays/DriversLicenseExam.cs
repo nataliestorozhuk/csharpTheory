e09748bef7b86529976f5578a36adf2d7c3cacbe
Nata <natalie.storozhuk@outlook.com>
2021-12-11 15:18:54 -0500

    completed DriversLicenseExam exercise

--- Arrays/DriversLicenseExam.cs
+++ Arrays/DriversLicenseExam.cs
@@ -1,27 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq; // for advance work with data
 
 namespace Arrays
 {
     class DriversLicenseExam
     {
-        public static void Run() {
+        public static void Run()
+        {
+            char[] correctAnswersArr = new char[] { 'B', 'D', 'A', 'A', 'C' };
+            char[] studentsAnswersArr = new char[5];
 
-          
+
+
+            //for (int i = 0; i < studentsAnswersArr.Length; i++)
+            //{
+            //    Console.WriteLine("Enter students answers (A, B, C or D): ");
+            //    string studentsAnswer = Console.ReadLine().ToUpper();
+            //    studentsAnswersArr[i] = Convert.ToChar(studentsAnswer);
+            //}
+
+             PassedWithLinq(correctAnswersArr, new char[] { 'B', 'B', 'A', 'A', 'C' });
+
+       
+
+            //Console.WriteLine(Passed(correctAnswersArr, studentsAnswersArr));
+            //Console.WriteLine("The total of correct answers is: " + TotalCorrect(correctAnswersArr, studentsAnswersArr));
+            //Console.WriteLine("The total of incorrect answers is: " + TotalIncorrect(correctAnswersArr, studentsAnswersArr));
+            //Console.WriteLine("The amount of missed questions is: " + QuestionMissed(correctAnswersArr, studentsAnswersArr));
+
         }
 
-        char[] correctAnswers = new char[] { 'B', 'D', 'A', 'A', 'C' };
-        char[]  studentsAnswers = new char[5];
 
-        //public static bool Passed(char[] studAnsw)
-        //{
 
-        //    return;
-        //}
-        private static int totalCorrect(char[] correctAnsw, char[] studAnswers)
+        public static bool PassedWithLinq(char[] cArr, char[] sArr)
+        {
+            // return cArr.Intersect(sArr).Count() == 4 ? true : false;
+
+            return cArr.Count(x => sArr.Contains(x)) == 4 ? true : false; 
+        }
+
+
+
+        public static bool Passed(char[] corrAnswers, char[] studAnsw)
+        {
+            bool results = false;
+            int counter = 0;
+
+            for (int i = 0; i < corrAnswers.Length; i++)
+            {
+
+                if (corrAnswers[i] == studAnsw[i])
+                {
+                    results = true;
+                    counter++;
+                    if (counter == 4)
+                        break;
+                }
+                else
+                {
+                    results = false;
+                }
+            }
+            return results;
+        }
+
+        public static int TotalCorrect(char[] correctAnsw, char[] studAnswers)
         {
             int numCorrect = 0;
 
@@ -30,6 +73,28 @@ namespace Arrays
                     numCorrect++;
 
             return numCorrect;
+        }
+        private static int TotalIncorrect(char[] correctAnsw, char[] studAnswers)
+        {
+            int numIncorrect = 0;
+
+            for (int i = 0; i < correctAnsw.Length; i++)
+                if (correctAnsw[i] != studAnswers[i])
+                    numIncorrect++;
+
+
+            return numIncorrect;
+        }
+
+        private static int QuestionMissed(char[] correctAnsw, char[] studAnswers)
+        {
+            int[] missedQuestions = new int[correctAnsw.Length];
+
+            for (int i = 0; i < correctAnsw.Length; i++)
+                if (correctAnsw[i] != studAnswers[i])
+                    missedQuestions[i] = (i + 1);
+
+            return missedQuestions.Length;
         }
 
     }
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -7,33 +7,14 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+
+
+
+            DriversLicenseExam.Run();
             //SamplesArray.Run();
             //LargerThanN.Run();
             //ChargeAccountValidation.Run();
             //RainfallClass.Run();
-
-
-        Point[] points = 
-                { 
-                    new Point(100, 200),
-                    new Point(150, 250), 
-                    new Point(250, 375),
-                    new Point(275, 395), 
-                    new Point(295, 450) 
-                };
-
-            // is greater than 100000.
-            Point first = Array.Find(points, ProductGT10);
-
-            // Display the first structure found.
-            Console.WriteLine("Found: X = {0}, Y = {1}", first.X, first.Y);
         }
-
-        // Return true if X times Y is greater than 100000.
-        static bool ProductGT10(Point p)
-        {
-            return p.X * p.Y > 100000;
-        }
-    
     }
 }
