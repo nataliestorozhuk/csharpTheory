c212e52495f0d2378bc9369f37fa49d3060aac1d
Nata <natalie.storozhuk@outlook.com>
2021-12-16 13:35:45 -0500

    added Question class

--- Arrays/Question.cs
+++ Arrays/Question.cs
@@ -13,6 +13,25 @@ namespace Arrays
         public string PossAnswer2 { get; set; }
         public string PossAnswer3 { get; set; }
         public string PossAnswer4 { get; set; }
+        public string[] NumberOfCorrectAnswers { get; set; }
 
+        public Question(string triviaQuestion, string possAnswer1, string possAnswer2, string possAnswer3, string possAnswer4, string[] numberOfCorrectAnswers)
+        {
+            TriviaQuestion = triviaQuestion;
+            PossAnswer1 = possAnswer1;
+            PossAnswer2 = possAnswer2;
+            PossAnswer3 = possAnswer3;
+            PossAnswer4 = possAnswer4;
+            NumberOfCorrectAnswers = numberOfCorrectAnswers;
+        }
+    }
+
+    class DemoQuestion
+    {
+        List<Question> questions = new List<Question>()
+        {
+            new Question("When do we celebrate independence day? ", "July 4th", "August 6th", "September 11th", 1),
+
+        };
     }
 }
