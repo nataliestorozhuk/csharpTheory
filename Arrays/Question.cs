dbdbe4cc3d766dcfd052236b841346b81b39ece2
Nata <natalie.storozhuk@outlook.com>
2021-12-23 13:54:48 -0500

    added new project Classes and Objects and new class Area

--- Arrays/Question.cs
+++ Arrays/Question.cs
@@ -15,44 +15,44 @@ namespace Arrays
             new Question("When do we celebrate independence day? ", "July 4th", "August 6th", "September 11th", 1),
             new Question("What is the capital of Ukraine?", "Kiev", "Oslo", "London",1),
             new Question("When do we celebrate Christmas?", "December 6th", "December 25th", "December 28th", 2),
-            new Question("What is the capital of UK?", "Kiev", "Oslo", "London", 3),
+            new Question("What is the capital of UK?", "Kiev", "Oslo", "London", 3) };
 
 
-        };
-            for (int i = 0; i < questions.Capacity; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine("Player one answer the question: \n");
-                    PrintQuestion(questions);
-                    String answer = Console.ReadLine();
-                    if (answer.ToUpper(questions(i).CorrectAnswer))
-                    {
-                        pointsForFirstPlayer++;
-                        Console.WriteLine($"Correct!!! You have {pointsForFirstPlayer} points.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Sorry, that was incorrect!!! You have {pointsForFirstPlayer} points.");
-                    }
-                   else
-                    {
-                        Console.WriteLine("Player two answer the question: \n");
-                        PrintQuestion(questions(i));
-                        String answer2 = Console.ReadLine();
-                        if (answer2.ToUpper(questions(i).CorrectAnswer))
-                        {
-                            pointsForSecondPlayer++;
-                            Console.WriteLine($"Correct!!! You have {pointsForSecondPlayer} points.");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Sorry, that was incorrect!!! You have {pointsForSecondPlayer} points.");
-                        }
-                    }
+        //};
+        //    for (int i = 0; i < questions.Capacity; i++)
+        //    {
+        //        if (i % 2 == 0)
+        //        {
+        //            Console.WriteLine("Player one answer the question: \n");
+        //            PrintQuestion(questions);
+        //            String answer = Console.ReadLine();
+        //            if (answer.ToUpper(questions(i).CorrectAnswer))
+        //            {
+        //                pointsForFirstPlayer++;
+        //                Console.WriteLine($"Correct!!! You have {pointsForFirstPlayer} points.");
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine($"Sorry, that was incorrect!!! You have {pointsForFirstPlayer} points.");
+        //            }
+        //           else
+        //            {
+        //                Console.WriteLine("Player two answer the question: \n");
+        //                PrintQuestion(questions(i));
+        //                String answer2 = Console.ReadLine();
+        //                if (answer2.ToUpper(questions(i).CorrectAnswer))
+        //                {
+        //                    pointsForSecondPlayer++;
+        //                    Console.WriteLine($"Correct!!! You have {pointsForSecondPlayer} points.");
+        //                }
+        //                else
+        //                {
+        //                    Console.WriteLine($"Sorry, that was incorrect!!! You have {pointsForSecondPlayer} points.");
+        //                }
+        //            }
 
-                }
-            }
+        //        }
+        //    }
 
             // Condition checks who wins.
             if (pointsForFirstPlayer > pointsForSecondPlayer)
---
+++ ClassesAndObjects/Area.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Area
+    {
+        public static void Run()
+        {
+
+        }
+
+        public static double GetArea(double rd)
+        {
+            return Math.PI * Math.Pow(rd, 2);
+        }
+        public static double GetArea(double width, double length)
+        {
+            return width * length;
+        }
+        public static double GetArea(double cylinderRadius, double height)
+        {
+            return Math.PI * Math.Pow(cylinderRadius, 2) * height;
+        }
+    }
+}
