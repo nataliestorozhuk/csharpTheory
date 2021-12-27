60bb89aeec0f07c9c83506c854faded148fd19ce
Nata <natalie.storozhuk@outlook.com>
2021-12-27 12:07:35 -0500

    added RoomDimension class

---
+++ ClassesAndObjects/RoomDimension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class RoomDimension
+    {
+        private double lenght;
+        private double width;
+
+        public RoomDimension(double lenght, double width)
+        {
+            this.lenght = lenght;
+            this.width = width;
+        }
+
+        public static double GetRoomArea(double l, double w)
+        {
+            return l * w;
+        }
+    }
+
+    class RoomCarpet
+    {
+        private RoomDimension size;
+        private double carpetCost;
+
+
+    }
+}
