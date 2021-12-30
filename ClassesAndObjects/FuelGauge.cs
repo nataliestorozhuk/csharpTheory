0689de5b3a6b3c20fdd7a3ffdd04875af6634339
Nata <natalie.storozhuk@outlook.com>
2021-12-30 13:47:04 -0500

    added FuelGauge and Odometer classes for car instrument simulator exercise

---
+++ ClassesAndObjects/FuelGauge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class FuelGauge
+    {
+        public static void Run()
+        {
+
+        }
+
+
+    }
+    class Odometer
+    {
+
+    }
+}
