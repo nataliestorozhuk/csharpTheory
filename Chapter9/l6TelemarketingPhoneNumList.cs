1c687312bc8a76e38ee8a848b01b5a0bc7ccfe7b
Nata <natalie.storozhuk@outlook.com>
2022-08-22 11:11:34 -0400

    added TelemarketingPhoneNumList exersice

--- Chapter9/l6TelemarketingPhoneNumList.cs
+++ Chapter9/l6TelemarketingPhoneNumList.cs
@@ -12,56 +12,21 @@ namespace Chapter9
         public static void Run()
         {
 
-            String[] name = { "Harrison, Rose", "James, Jean", "Smith, William", "Smith, Brad" };
+            String[] names = { "Harrison, Rose", "James, Jean", "Smith, William", "Smith, Brad" };
             String[] phoneNum = { "555-2234", "555-9098", "555-1785", "555-9224" };
 
-            String nameEntered = "Smith";
+            String[] result = Array.FindAll(names, e => e.ToLower().StartsWith("smi"));
 
-            for (int i = 0; i < name.Length; i++)
+            foreach(String n in result)
             {
-if (name[i] == nameEntered)
-                {
-                    Console.WriteLine();
-                }
-                for (int j = 0; j < phoneNum.Length; j++)
-                {
-                    if( name[i]== 0)
-                    {
-                        Console.WriteLine(phoneNum[j]);
-                    }
-                }
+                var x = Array.IndexOf(names, n);
+                var y = phoneNum[x];
+
+                Console.WriteLine(n + "    " + y);
             }
+
 
         }
 
-
-
-
-
-        //List<PhoneNumberList> phones = new List<PhoneNumberList>()
-        //    {
-
-        //        new PhoneNumberList("Harrison", "Rose", "555-2234"),
-        //        new PhoneNumberList("James","Jean", "555-9098"),
-        //        new PhoneNumberList("Smith","William", "555-1785"),
-        //        new PhoneNumberList("Smith","Brad", "555-9224")
-        //    };
-
     }
-
-
-    //public class PhoneNumberList
-    //{
-
-    //    public string firstName;
-    //    public string lastName;
-    //    public string phoneNumber;
-
-    //    public PhoneNumberList(string firstName, string lastName, string phoneNumber)
-    //    {
-    //        this.firstName = firstName;
-    //        this.lastName = lastName;
-    //        this.phoneNumber = phoneNumber;
-    //    }
-    //}
 }
