3fe0933b25aa7dc9cac4f4d1f030a3e4c9fabc8f
Nata <natalie.storozhuk@outlook.com>
2022-09-01 14:35:16 -0400

    completed l12MiscellaneousStringOperations exercise

--- Chapter9/l12MiscellaneousStringOperations.cs
+++ Chapter9/l12MiscellaneousStringOperations.cs
@@ -11,11 +11,18 @@ namespace Chapter9
         public static void Run()
         {
             string text = "hello a beautiful world";
-
             Console.WriteLine(GetWordCount(text));
 
             char[] chars = { 'N', 'a', 't', 'a', 'l', 'i', 'y', 'a' };
             Console.WriteLine(ArrayToString(chars));
+
+            string words = "nataliya";
+            Console.WriteLine(GetMostFrequent(words));
+
+            string string1 = "the dog jumped over the fence";
+            string string2 = "the";
+            string string3 = "that";
+            Console.WriteLine(GetReplacedSubstring(string1, string2, string3));
         }
 
         public static int GetWordCount(string word)
@@ -30,5 +37,41 @@ namespace Chapter9
 
           return new string(chars);
         }
+
+        public static char GetMostFrequent(string word)
+        {
+            string mostFrequent = "";
+            int count = 0;
+
+            String[] words = word.Split(" ");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int temp = 0;
+
+                for (int j = 0; j < words.Length; j++)
+                {
+
+                    if (words[i].Equals(words[j].ToLower()))
+                    {
+                        temp++;
+                    }
+
+                    if (temp > count)
+                    {
+                        count = temp;
+                        mostFrequent = words[i];
+                    }
+                }
+            }
+            return mostFrequent[0];
+        }
+
+        public static string GetReplacedSubstring(string str1, string str2, string str3)
+        {
+
+           return str1.Replace(str2,str3);
+        }
+
     }
 }
