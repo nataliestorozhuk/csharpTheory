696d63b5725b851db921abdbddf67481355b04be
Nata <natalie.storozhuk@outlook.com>
2023-01-26 12:13:53 -0500

    added AlphabeticTelephoneNumberTranslator exercise

--- Chapter9/l13AlphabeticTelephoneNumberTranslator.cs
+++ Chapter9/l13AlphabeticTelephoneNumberTranslator.cs
@@ -11,7 +11,21 @@ namespace Chapter9
 
         public static void Run()
         {
+            Console.Write("Enter a telephone number (xxx-xxx-xxxx): ");
+            string number = Console.ReadLine();
 
+            // Create a dictionary to store the numeric equivalents of alphabetic characters
+            var letterToNumber = new Dictionary<char, int>
+        {
+            { 'a', 2 }, { 'b', 2 }, { 'c', 2 },
+            { 'd', 3 }, { 'e', 3 }, { 'f', 3 },
+            { 'g', 4 }, { 'h', 4 }, { 'i', 4 },
+            { 'j', 5 }, { 'k', 5 }, { 'l', 5 },
+            { 'm', 6 }, { 'n', 6 }, { 'o', 6 },
+            { 'p', 7 }, { 'q', 7 }, { 'r', 7 }, { 's', 7 },
+            { 't', 8 }, { 'u', 8 }, { 'v', 8 },
+            { 'w', 9 }, { 'x', 9 }, { 'y', 9 }, { 'z', 9 }
+        };
 
         }
     }
