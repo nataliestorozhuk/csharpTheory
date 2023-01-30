f46fe8a5046ef2b010e728f5e5ee4349289d6dba
Nata <natalie.storozhuk@outlook.com>
2023-01-30 12:18:06 -0500

    added PersonAndCustomerClasses exercise

---
+++ Chapter9/l10PersonAndCustomerClasses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    internal class l10PersonAndCustomerClasses
+    {
+    }
+}
