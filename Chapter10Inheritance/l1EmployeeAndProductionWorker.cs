e258ee8071cfd75fba5cd3309a0a6d676ab7c9dc
Nata <natalie.storozhuk@outlook.com>
2023-01-31 12:28:17 -0500

    added isValidEmpNum method

--- Chapter10Inheritance/l1EmployeeAndProductionWorker.cs
+++ Chapter10Inheritance/l1EmployeeAndProductionWorker.cs
@@ -8,5 +8,38 @@ namespace Chapter10Inheritance
 {
     internal class l1EmployeeAndProductionWorker
     {
+
+        public string EmployeeName { get; set; }
+        public string EmployeeNumber { get; set; }
+        public string HireDate { get; set; }
+
+
+        private bool isValidEmpNum(String e)
+        {
+            bool status = true;
+
+            if (e.Length != 5)
+                status = false;
+            if (e[3] != '-') return false;
+
+            int numberPart;
+            if (!int.TryParse(e.Substring(0, 3), out numberPart)) return false;
+            if (numberPart < 100 || numberPart >= 1000) return false;
+
+            char letterPart = e[4];
+            if (letterPart < 'A' || letterPart > 'M') return false;
+
+
+            return status;
+        }
+
+        private class ProductionWorker: l1EmployeeAndProductionWorker
+       {
+            public int Shift { get; set; }
+            public double HourlyPayRate { get; set; }
+
+
+
+        }
     }
 }
