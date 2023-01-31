ac129b7216a3a9001abb39cdedd3c4bb4855b15e
Nata <natalie.storozhuk@outlook.com>
2023-01-31 14:41:18 -0500

    added some changes

--- Chapter10Inheritance/l1EmployeeAndProductionWorker.cs
+++ Chapter10Inheritance/l1EmployeeAndProductionWorker.cs
@@ -86,6 +86,27 @@ namespace Chapter10Inheritance
             }
         }
 
+        public static void Run()
+        {
+            ProductionWorker pw =
+                        new ProductionWorker("John Smith", "123-A", "11-15-2005",
+                                                           ProductionWorker.DAY_SHIFT, 16.50);
+
+            Console.WriteLine("Here's the first production worker. ");
+            Console.WriteLine(pw);
+
+            ProductionWorker pw2 = new ProductionWorker();
+            pw2.setName("Joan Jones");
+            pw2.setEmployeeNumber("222-L");
+            pw2.setHireDate("12-12-2005");
+            pw2.setShift(ProductionWorker.NIGHT_SHIFT);
+            pw2.setPayRate(18.50);
+
+            Console.WriteLine("\nHere's the second production worker. ");
+            Console.WriteLine(pw2);
+        }
 
     }
+
+    
 }
