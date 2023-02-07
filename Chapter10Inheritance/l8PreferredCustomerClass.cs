90066d130cac9b44b3e43ebb0f1817e4b8086220
Nata <natalie.storozhuk@outlook.com>
2023-02-07 12:40:37 -0500

    added l8PreferredCustomerClass exercise

--- Chapter10Inheritance/l8PreferredCustomerClass.cs
+++ Chapter10Inheritance/l8PreferredCustomerClass.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Chapter10Inheritance
 {
-    internal class l8PreferredCustomerClass
+    internal class l8PreferredCustomerClass: Customer
     {
+        public static readonly double FivePercent = 0.05;
+        public static readonly double SixPercent = 0.06;
+        public static readonly double SevenPercent = 0.07;
+        public static readonly double TenPercent = 0.10;
+
+
+        public double PurchaseAmount { get; set; }
+
+        public l8PreferredCustomerClass(string name, string address, string telephone, int customerNumber, bool onMailingList) : base(name, address, telephone, customerNumber, onMailingList)
+        {
+        }
+
+        public static void Run()
+        {
+              
+
+        }
     }
 }
