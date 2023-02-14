1178daafd036f3147619f8c59f3cce268ca9c7ca
Nata <natalie.storozhuk@outlook.com>
2023-02-14 09:35:32 -0500

    completed l9BankAccount class

--- Chapter10Inheritance/l9BankAccount.cs
+++ Chapter10Inheritance/l9BankAccount.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace Chapter10Inheritance
 {
-    internal class l9BankAccount
+    internal abstract class l9BankAccount
     {
+        public double Balance { get; set; }
+        public int NumOfDepositsPerMonth { get; set; }
+        public int NumOfWithdrawalsPerMonth { get; set; }
+        public double AnnualInterestRate { get; set; }
+        public double MonthlyServiceCharges { get; set; }
+
+        protected l9BankAccount(double balance, double interestRate)
+        {
+            Balance = balance;
+            AnnualInterestRate = interestRate;
+        }
+
+        
+        public void deposit(double amount)
+        {
+            Balance += amount;
+            NumOfDepositsPerMonth++;
+        }
+
+        
+        public void withdraw(double amount)
+        {
+            Balance -= amount;
+            NumOfWithdrawalsPerMonth++;
+            
+        }
+
+        public void calcInterest()
+        {
+            double monthlyInterest = AnnualInterestRate / 12;
+             monthlyInterest = Balance * monthlyInterest;
+            Balance += monthlyInterest;
+           
+        }
+
+        public void monthlyProcess()
+        {
+            Balance -= MonthlyServiceCharges;
+            calcInterest();
+
+            NumOfDepositsPerMonth = 0;
+            NumOfWithdrawalsPerMonth = 0;
+            MonthlyServiceCharges = 0;
+           
+        }
     }
+
 }
